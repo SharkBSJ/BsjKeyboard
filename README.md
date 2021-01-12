ed9f7ae2c4f07ff0cd7d7c0dab591031c66f637c
SharkBSJ <baesungjin1996@gmail.com>
2021-01-12 22:22:46 +0900

    First Commit

---
+++ README.md
@@ -0,0 +1,11 @@
+## 프로젝트 소개
+
+- I made custom keyboard that could be used only in Android (Because I should use it in my company)
+- Thanks a lot
+- About Keyboard Setting In Android Refer to below href  
+    https://www.androidcentral.com/how-set-default-keyboard-your-android-phone  
+    https://www.androidcentral.com/how-change-terrible-keyboard-your-samsung-galaxy-phone
+
+- I referred below href for making Keyboard APP  
+    https://www.androidauthority.com/lets-build-custom-keyboard-android-832362/  
+    https://stackoverflow.com/questions/9577304/how-can-you-make-a-custom-keyboard-in-android
---
+++ app/src/main/java/kr/sjsoft/bsjkeyboard/MainActivity.java
@@ -0,0 +1,23 @@
+package kr.sjsoft.bsjkeyboard;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    // About Keyboard Setting In Android
+    // Refer to below href
+    // https://www.androidcentral.com/how-set-default-keyboard-your-android-phone
+    // https://www.androidcentral.com/how-change-terrible-keyboard-your-samsung-galaxy-phone
+
+    // I referred below href for making Keyboard APP
+    // https://www.androidauthority.com/lets-build-custom-keyboard-android-832362/
+    // https://stackoverflow.com/questions/9577304/how-can-you-make-a-custom-keyboard-in-android
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
